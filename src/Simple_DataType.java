252f0f69e34ac2791ce36b18407efc106049bf31
GoreNeko <85627591+GoreNeko@users.noreply.github.com>
2022-07-24 22:29:16 +0900

    Simple_DataType

---
+++ src/Simple_DataType.java
@@ -0,0 +1,17 @@
+public class Simple_DataType {
+    public static void main(String[] args) {
+        boolean isFun = true;
+        System.out.print(!isFun);
+
+        char c = 'f'; // 한 개의 문자 표현 가능
+        int x = 59;
+        long a = 1981968549L; // 마지막에 L
+        float f = 89.41f; // 마지막에 f
+        double d = 19819.7419871;
+        System.out.println(c);
+        System.out.println(x);
+        System.out.println(a);
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
