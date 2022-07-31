5d5c7d4fc94c31e4cc58d3e57f70f42cee0bbdbd
GoreNeko <85627591+GoreNeko@users.noreply.github.com>
2022-07-31 15:36:42 +0900

    boolean 연산자

---
+++ src/booleanOperator.java
@@ -0,0 +1,16 @@
+public class booleanOperator {
+    public static void main(String[] args){
+        int i = 6;
+        int j = 6;
+        System.out.println(i == j);
+        System.out.println(i != j);
+
+        int a = 30;
+        int b = 7;
+        int c = 7;
+        System.out.println(a < b);
+        System.out.println(a > b);
+        System.out.println(b <= c);
+        System.out.println(b >= c);
+    }
+}
